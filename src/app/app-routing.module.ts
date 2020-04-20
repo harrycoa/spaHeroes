63faa5017c627db6aedab4c92e5130a18ed913b7
Harry Coa <rodol_2000@hotmail.com>
2020-04-19 23:27:00 -0500

    agregamos routin app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component'
 
+const routes: Routes = [
+{ path: 'home', component: HomeComponent}
+,{ path: '**', pathMatch:'full', redirectTo:'home'}
 
-const routes: Routes = [];
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
