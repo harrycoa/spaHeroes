052d905339d00b6e19e0875491d44f71fbd08e04
Harry Coa <rodol_2000@hotmail.com>
2020-05-18 00:07:54 -0500

    ejemplo ng if y pipes basicos

--- src/app/components/heroe/heroe.component.ts
+++ src/app/components/heroe/heroe.component.ts
@@ -1,22 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { HeroesService } from '../../servicios/heroes.service';
 
 
 @Component({
   selector: 'app-heroe',
   templateUrl: './heroe.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class HeroeComponent implements OnInit {
-
-  constructor( private activatedRoute: ActivatedRoute) { 
-    this.activatedRoute.params.subscribe( params => {
-      console.log( params );
+  heroe: any = {};
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private heroesService: HeroesService
+  ) {
+    this.activatedRoute.params.subscribe((params) => {
+      this.heroe = this.heroesService.getHeroe(params['id']);
+      console.log(this.heroe);
     });
   }
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
